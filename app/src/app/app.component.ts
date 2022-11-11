ad9aeccacf6dd84f7398ed1a7defcf05b2f76a27
MarkacRobi <robi.markac@gmail.com>
2022-11-11 13:46:17 +0100

    integration of phaser into Angular app

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Phaser from 'phaser';
+
+class NewScene extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'new' });
+  }
+
+  preload() {
+    console.log('enter preload');
+  }
+
+  create() {
+    console.log('enter create');
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  phaserGame?: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      scene: [ NewScene ],
+      physics: {
+        default: 'arcade',
+      },
+      scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'gameContainer',
+        width: 800,
+        height: 600
+      }
+    };
+  }
+
+  ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+
 }
